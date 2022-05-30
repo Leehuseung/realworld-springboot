a680e8fe31e1cd5fdd634f7bb112ef2c890dfc1f
Leehuseung <gntmd4261@naver.com>
2022-05-31 00:03:24 +0900

    RealWorld api에 맞게 request body에 json을 이용하여 login 하도록 구현

--- src/main/java/com/kr/realworldspringboot/security/filter/ApiLoginFilter.java
+++ src/main/java/com/kr/realworldspringboot/security/filter/ApiLoginFilter.java
@@ -1,10 +1,10 @@
 package com.kr.realworldspringboot.security.filter;
 
-import com.kr.realworldspringboot.entity.Member;
 import com.kr.realworldspringboot.security.dto.AuthMemberDTO;
 import com.kr.realworldspringboot.util.JWTUtil;
 import lombok.extern.log4j.Log4j2;
 import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Log4j2
 public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
@@ -33,9 +34,14 @@ public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        String body = request.getReader().lines().collect(Collectors.joining());
 
-        String email = request.getParameter("email");
-        String pw = request.getParameter("password");
+        JSONObject jo = (JSONObject) JSONValue.parse(body);
+        JSONObject user = (JSONObject) jo.get("user");
+
+
+        String email = user.getAsString("email");
+        String pw = user.getAsString("password");
         //email이 존재하는지 검사한다.
         if(email == null){
             throw new BadCredentialsException("email cannot be null");
@@ -48,23 +54,23 @@ public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-
         JSONObject jo = new JSONObject();
 
-        String email = ((AuthMemberDTO)authResult.getPrincipal()).getEmail();
-        String username = ((AuthMemberDTO)authResult.getPrincipal()).getUsername();
+        AuthMemberDTO memberDTO = (AuthMemberDTO)authResult.getPrincipal();
+
 
         String token = null;
 
         try {
-            token = jwtUtil.generateToken(email);
-
-            response.setContentType("application/json");
+            token = jwtUtil.generateToken(memberDTO.getEmail());
 
             jo.put("token",token);
-            jo.put("email",email);
-            jo.put("username",username);
+            jo.put("email",memberDTO.getEmail());
+            jo.put("username",memberDTO.getUsername());
+            jo.put("bio",memberDTO.getBio());
+            jo.put("image",memberDTO.getImage());
 
+            response.setContentType("application/json");
             response.getOutputStream().write(jo.toString().getBytes());
 
         } catch( Exception e) {
